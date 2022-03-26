19acccfedc5554e2ab3a8b42507e3ab0800f9bed
LuisLamiral8 <luislamiraal@gmail.com>
2022-03-26 19:06:45 -0300

    about-me

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Typical from "react-typical";
 import "./styles/globals.scss";
+
 function App() {
+  useEffect(() => {
+    document.title = "Luis's Portfolio";
+  }, []);
   return (
     <body className="body">
       <header className="title-container">
         <div className="title">
           {/* <h1>Luis Lamiral </h1> */}
-        <Typical
+          <Typical
             steps={["Luis Lamiral", 2500, "Frontend Developer", 500]}
             loop={Infinity}
             wrapper="h1"
           />
-          <h3>MY PORTFOLIO</h3>
+          <h2>MY PORTFOLIO</h2>
         </div>
       </header>
       <div className="wave-container">
         <img className="wave" width="100%" src="/wave.svg" alt="" />
       </div>
       <section className="desc">
-        <p>Lorem ip adipisci error eaque, consequatur laboriosam ipsam distinctio harum quos repellat accusamus vel alias similique doloremque corrupti! Corporis temporibus harum asperiores pariatur nobis, aspernatur aperiam repellat quas quibusdam? Quae aspernatur iusto tempora est tempore, aliquid amet iste blanditiis dignissimos voluptates cupiditate commodi. Eligendi, quas, tenetur sapiente molestias soluta sint, odio doloribus exercitationem aliquam magnam laborum id provident! Tempore voluptatibus facere consequuntur, debitis magnam quia perspiciatis quae perferendis? Est labore aspernatur quia. Facilis tempora ab, dignissimos sequi at mollitia nobis? Tempora neque aliquam quod ut laborum laudantium porro cum. Dolore reprehenderit explicabo rem odit ea veritatis ipsa dolores. Adipisci dolores omnis exercitationem. Est harum a sint suscipit molestiae dolore adipisci praesentium officiis voluptatibus eum modi velit, voluptas optio tempore deleniti numquam fugiat eveniet dicta recusandae, necessitatibus quia iusto ducimus. Explicabo quaerat aliquid temporibus eaque debitis incidunt ad cum molestiae officia, architecto voluptate reiciendis cumque dicta quo nisi optio adipisci, accusamus maiores ut eos error vel quas neque? Beatae veniam neque dolorum quidem cupiditate quasi natus nulla, minus earum, impedit commodi totam quae voluptas numquam officiis ex nobis! Impedit a dolorum voluptates explicabo fuga reiciendis eligendi maiores sunt debitis veritatis ea, quis doloremque voluptatem numquam repellendus illo. Error doloremque neque minus eos, provident dicta cupiditate rerum est totam atque odit aut impedit praesentium dolorum ullam numquam molestiae quasi a, recusandae accusantium reprehenderit soluta magni. Amet consequuntur pariatur odit fugiat provident. Quo, iure. Veritatis, voluptas! Dolor reprehenderit quam nam cumque id eius illum unde laboriosam doloremque ipsa, distinctio quidem quos veniam, aliquid dolorum in quaerat? Eligendi rerum enim eos ab, itaque sint eaque modi ea dolor numquam magni libero nulla ipsa dolorum quaerat, nisi asperiores ut iste doloremque saepe praesentium molestiae! Nemo quos accusamus non illo, iusto nihil quis totam molestiae nulla rerum reprehenderit ad aliquam libero voluptas quae aut corrupti optio necessitatibus sed saepe numquam inventore autem debitis fugit! Fugit enim, commodi nisi nobis dolorem nam ratione nihil ullam accusantium ipsam, accusamus repellat illum aliquid quae? Earum, cumque sequi. Ipsum beatae ab ipsa vitae temporibus, delectus incidunt quis tempore nostrum iste a illo earum voluptas provident possimus nesciunt dicta unde explicabo aliquid nulla veritatis iure reiciendis quibusdam repellendus? Iste praesentium incidunt repudiandae excepturi.</p>
+        <div className="desc_info-container">
+          <div className="esp">
+            <h3> Español </h3>
+            <p>
+              Mi nombre es Luis Lamiral <br />
+              Actualmente vivo en Argentina, Buenos Aires <br />
+              Me considero desarrollador web desde 2018, y busco trabajos en
+              freelancer en mi tiempo libre para aumentar mis ganancias y tener
+              una gran lista de clientes de una manera global. <br />
+              Tengo un gran manejo sobre el desarrollo y diseño de sitios web,
+              con tecnologías como por ejemplo: HTML5, CSS, JS, ReactJS, NextJS,
+              NodeJS, etc.
+            </p>
+          </div>
+          <div className="info-svg">
+            <img width="100%" height="100%" src="/text.svg" alt="" />
+          </div>
+          <div className="eng">
+            <h3> English </h3>
+            <p>
+              My name is Luis Lamiral <br />
+              I live in Argentina, Buenos Aires <br />
+              I'm a web developer since 2018. I am looking for freelance
+              developer jobs in my spare time, to increase my earnings and gain
+              a large list of clients around the world. <br /> I have a great
+              handle on the development and design of websites, with
+              technologies such as: HTML5, CSS, JS, ReactJS, NextJS, NodeJS,
+              etc.
+            </p>
+          </div>
+        </div>
       </section>
     </body>
   );
