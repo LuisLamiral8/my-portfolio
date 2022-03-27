ee9f944586c2fd47d65c42c768e6ea1ad8c6a0eb
LuisLamiral8 <luislamiraal@gmail.com>
2022-03-26 23:32:17 -0300

    tech

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Typical from "react-typical";
+import Slider from './components/Slider/ImageSlider';
 import "./styles/globals.scss";
-
 function App() {
   useEffect(() => {
     document.title = "Luis's Portfolio";
@@ -55,13 +55,23 @@ function App() {
           </div>
         </div>
       </section>
-      <section className="techs">
-      <div className="wave_tech-container">
+      {/* <div className="wave">
         <img className="wave" width="100%" src="/wave_info-tech_up.svg" alt="" />
-      </div>
-        <h1>
-          holaaaaaaa
-        </h1>
+      </div> */}
+      <section className="techs">
+        <div className="tech-title">
+        <h3>Tecnologías</h3>
+        <h3>Technologies</h3>
+        </div>
+        {/* <Slider></Slider> */}
+        <div className="img-container">
+          <img className="img1"src="/slider/1.svg" width="150px" heigt="150px" alt="" />
+          <img className="img2"src="/slider/2.svg" width="150px" heigt="150px" alt="" />
+          <img className="img1"src="/slider/3.svg" width="175px" heigt="150px" alt="" />
+          <img className="img2"src="/slider/4.svg" width="175px" heigt="150px" alt="" />
+          <img className="img1"src="/slider/5.svg" width="150px" heigt="150px" alt="" />
+          <img className="img2"src="/slider/6.svg" width="150px" heigt="150px" alt="" />
+        </div>
       </section>
     </body>
   );
